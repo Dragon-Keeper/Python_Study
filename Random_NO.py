406d9cb2dde8bc857068a05775c623f910ac647b
Dragon-Keeper <44353148+Dragon-Keeper@users.noreply.github.com>
2018-10-31 23:53:54 +0800

    成功将随机数写入数据文件

--- Random_NO.py
+++ Random_NO.py
@@ -1,5 +1,8 @@
 # coding: UTF-8
 import random  # 导入随机函数random模块，这样下面才能使用random函数产生随机数
+import os  # 导入os模块，os模块包含普遍的操作系统功能
+
+set_no = set()  # 生成空白集合，否则后面生成的随机数无法合法写入集合，因为集合还没有被定义
 
 try:  # try/except语句用来检测try语句块中的错误，从而让except语句捕获异常信息并处理。
     f = open('D://old_no.txt')  # 测试能否打开这个文件来判断文件是否存在
@@ -8,34 +11,52 @@ except IOError:  # 判断是否出现IOError这个错误信息，如果出现，
     print("此前的随机数据不存在，现在将新建随机数据储存文件。")
     file = open('D://old_no.txt', 'a')  # 新建一个数据储存文件，参数a表示打开一个文件用于追加。如果该文件已存在，文件指针将会放在文件的结尾。也就是说，新的内容将会被写入到已有内容之后。如果该文件不存在，创建新文件进行写入。
 else:  # 如果上面判断语句没发现IOError这个错误信息，则运行以下内容
+
     # -------------------------------此处开始为读取文件内容并生成列表，然后将str的列表转换成int格式--------------------------------------------------
-    file_read = open('D://old_no.txt')  # 再次打开文件读取文件内容，如果直接用file_read的话，内容为空，因为前面已经使用了
-    list_no = []  # 建立空白列表
-    for line in file_read.readlines():  # 读取文件内容并且用reallines()函数生成列表形式
-        list_no.append(list(map(int, line.split(','))))   # 将每行内容添加进列表，用'，'隔开，这样生成的列表里面每个字符都是子列表
-    # print("list_no={}".format(list_no))
-    list_no_change = str(list_no).replace('[', '').replace(']', '').replace(' ', '')  # 用replace函数将子列表的"["&"]"&"空格"去掉，但是列表每个值还是字符串形式
-    # print(list_no_change)  # 输出字符串
-    list_no_ok = list(eval(list_no_change))  # 应用强大的eval函数转义将字符串型转换成数据型，然后再将转义后的列表用list函数生成列表，再保存替换原来的列表中
-    # print('之前保存的数据为:{}'.format(list_no_ok))  # 输出int格式的列表
-    # print("{}的格式为{}".format(list_no_ok[0],type(list_no_ok[0])))# 显示列表第一个值的格式为int
-    set_no = set(list_no_ok)
-    print('之前保存的数据为:{}'.format(set_no))  # 输出int格式的集合
-    file_read.close()  # 文件打开了就必须要关闭-----------------------------自此，成功将文件内容读出并转换为int格式的集合-----------------------------
+
+    # 以下代码为判断文件内容是否为空，如果是空的话则跳过下面的步骤，直接到输入起始数字位置--------------------------------------------------------------
+    size = os.path.getsize('D://old_no.txt')  # 判断文件内容是否为空
+    if size != 0:  # 判断文件内容不为空，则打开文件读取内容并转化成int格式集合
+        file_read = open('D://old_no.txt')  # 再次打开文件读取文件内容，如果直接用file_read的话，内容为空，因为前面已经使用了
+        list_no = []  # 建立空白列表
+        for line in file_read.readlines():  # 读取文件内容并且用reallines()函数生成列表形式
+            list_no.append(list(map(int, line.split(','))))   # 将每行内容添加进列表，用'，'隔开，这样生成的列表里面每个字符都是子列表
+            # print("list_no={}".format(list_no))
+        list_no_change = str(list_no).replace('[', '').replace(']', '').replace(' ', '')  # 用replace函数将子列表的"["&"]"&"空格"去掉，但是列表每个值还是字符串形式
+        # print(list_no_change)  # 输出字符串
+        list_no_ok = list(eval(list_no_change))  # 应用强大的eval函数转义将字符串型转换成数据型，然后再将转义后的列表用list函数生成列表，再保存替换原来的列表中
+        # print('之前保存的数据为:{}'.format(list_no_ok))  # 输出int格式的列表
+        # print("{}的格式为{}".format(list_no_ok[0],type(list_no_ok[0])))# 显示列表第一个值的格式为int
+        set_no = set(list_no_ok)
+        print('之前保存的数据为:{}'.format(set_no))  # 输出int格式的集合
+        file_read.close()  # 文件打开了就必须要关闭-----------------------------自此，成功将文件内容读出并转换为int格式的集合-------------------------
+    else:  # 判断文件内容为空，直接到输入起始数字位置开始
+        pass
 
 '''
     ------------------------------------------------在界面角落提供“重置”按钮，如果要清除之前的数据请按按钮-------------------------------------
 print("确认要重置数据吗？是/否")#按下按钮后，弹出确认框，是对应True，否对应False，并赋值给reset_button按钮
-if reset_button==True:
-    file=open('D://old_no.txt','w')
+if reset_button is True:
+    file = open('D://old_no.txt', 'w')
     file.close()
-elif reset_button==False:
-    pass
+else:
+    if reset_button is False:
+        pass
     -----------------------------------------------------------------以上为多行注释来屏蔽GUI界面的功能----------------------------------------
 '''
 '''
     ---------------------------------每次随机数据暂存内存，需要的话点击“保存”按钮写入到文件里，下次打开再点击读取---------------------------------
-file=open('D://old_no.txt','a')#打开数据文件准备写入，参数a表示新内容将会被写入到已有内容之后
+if save_button is True:
+    list_set = list(set_no)  # 将集合转换成列表
+    file = open('D://old_no.txt', 'w')  # 将数据处理后覆盖写入文件
+    for i in range(len(list_set)):  # 用len()计算列表list_set的长度，并且历遍列表list_set
+        s = str(list_set[i]).replace('[', '').replace(']', '')  # 去除[],这两行按数据不同，可以选择
+        s = s.replace("'", '').replace(',', '') + '\n'  # 去除单引号，逗号，每行末尾追加换行符
+        file.write(s)
+    file.close()
+else:
+    if save_button is False:
+        pass
     ----------------------------------------------------------------以上为多行注释来屏蔽GUI界面的功能-----------------------------------------
 '''
 
@@ -59,14 +80,14 @@ random_no = random.randint(min_no, max_no)  # 产生范围内的随机数
 
 if random_no not in set_no:  # 判断随机数是否已存在，如果不存在则写入集合
     print('新的随机数生成：' + str(random_no))  # 调试用的输出
-    set_no.add('random_no')  # 随机数写入集合
+    set_no.add(random_no)  # 随机数写入集合
 else:
     while random_no in set_no:  # 判断随机数是否已存在，如果存在则重新产生随机数直到不存在
         print(str(random_no) + '：已经存在')  # 调试用的输出
         random_no = random.randint(min_no, max_no)  # 产生范围内的随机数
     else:  # 经过循环重新产生不存在的随机数，然后写入集合
         print('新的随机数生成：' + str(random_no))  # 调试用的输出
-        set_no.add('random_no')  # 随机数写入集合
+        set_no.add(random_no)  # 随机数写入集合
 # -----------------------------------------------自此，成功产生随机数并写入集合--------------------------------------------------
 '''
      -----------------------------------------------每次点击“随机”按钮运行下面代码产生随机数并写入集合内-----------------------------
@@ -81,7 +102,17 @@ else:
         random_no = random.randint(min_no, max_no)  # 产生范围内的随机数
     else:  # 经过循环重新产生不存在的随机数，然后写入集合
         print('新的随机数生成：' + str(random_no))  # 调试用的输出
-        set_no.add('random_no')  # 随机数写入集合
+        set_no.add(random_no)  # 随机数写入集合
     --------------------------------------------------以上为多行注释来屏蔽GUI界面的功能---------------------------------------------
 '''
 # 以下编写将集合写入数据文件的代码
+list_set = list(set_no)  # 将集合转换成列表
+print(list_set)
+
+file = open('D://old_no.txt', 'w')
+for i in range(len(list_set)):  # 用len()计算列表list_set的长度，并且历遍列表list_set
+    s = str(list_set[i]).replace('[', '').replace(']', '')  # 去除[],这两行按数据不同，可以选择
+    s = s.replace("'", '').replace(',', '') + '\n'  # 去除单引号，逗号，每行末尾追加换行符
+    file.write(s)
+file.close()
+# -----------------------------------------------自此，成功将随机数写入数据文件--------------------------------------------------
