a38e8f5efee348bea7a4568f8be2dc46a84ac5d3
Dragon-Keeper <44353148+Dragon-Keeper@users.noreply.github.com>
2018-11-16 00:59:56 +0800

    测试导入Line画直线

--- test-1.py
+++ test-1.py
@@ -1,33 +1,57 @@
 from kivy.app import App
-from kivy.graphics import Color, Rectangle
+from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.graphics.vertex_instructions import Rectangle, Ellipse, Line #导入矩形、椭圆、直线
+from kivy.graphics.context_instructions import Color #导入颜色控件
+from kivy.lang import Builder
+import random
 
-class RootWidget(FloatLayout):
-    def __init__(self, **kwargs):
-        super(RootWidget, self).__init__(**kwargs)
+Builder.load_string('''
+<ScatterTextWidget>:
+    orientation: 'vertical'
+    canvas: #创建画板
+        Color:
+            rgba: 0,0,1,1 #设置rgba色值
+        Ellipse: #椭圆
+            pos: 0, 400
+            size: 200, 100
+        Line: #直线段
+            points: [0,0,500,600,400,300]
+            close: True #直线闭合
+            width: 3 #线条宽度
+    TextInput:
+        id: my_textinput
+        font_size:150
+        size_hint_y: None
+        height: 200
+        text: 'default'
+    FloatLayout:
+        Scatter:
+            Label:
+                id: my_label
+                text: my_textinput.text
+                font_size: 150
+                on_text: root.color_change()
+    BoxLayout:
+        orientation: 'horizontal'
+        size_hint_y: None
+        height: 150
+        Label:
+            text: my_textinput.text[:3][::-1]
+            font_size: 100
+        Label:
+            text: my_textinput.text[-3:][::-1]
+            font_size: 100
+''')
+class ScatterTextWidget(BoxLayout):
+    def color_change(self):
+        label = self.ids['my_label']
+        label.color = [random.random() for _ in range(3)] + [1]
 
-        #添加一个按钮到布局
-        self.add_widget(
-            Button(
-                text = 'Hello World'
-                size_hint= (1, .5)
-                pos_hint = {'center_x': .5, 'center_y':.5}
-            )
-        )
-
+class TutorialApp(App):
     def build(self):
-        self.root = root = RootWidget()
-        root.bind(size=self._update_rect, pos=self._update_rect)
+        return ScatterTextWidget()
 
-        with root.canvas.before:
-            Color(0, 1, 0, 1)
-            self.rect = Rectangle(size = root.size, pos=root.pos)
-        return root
-
-    def _update_rect(self, instance, value):
-        self.rect.pos = instance.pos
-        self.rect.size = instance.size
-
-if __name__ = '__main__':
-    MainApp().run()
+TutorialApp().run()
