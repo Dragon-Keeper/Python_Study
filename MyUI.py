0ac706ee7568c283496aeab741ecb5cf3f539e12
Dragon-Keeper <44353148+Dragon-Keeper@users.noreply.github.com>
2018-11-20 13:59:59 +0800

    UI基本完成

--- MyUI.py
+++ MyUI.py
@@ -5,7 +5,7 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
 from kivy.graphics import Color, Rectangle # 为背景添加颜色，画矩形
-
+from kivy.config import Config
 
 
 Builder.load_string('''
@@ -37,44 +37,68 @@ Builder.load_string('''
                 pos:0, 0
                 size:338, 600
 
-#  以下为显示随机数结果画布
+#  标题画布上面留白20的高度给电量、时间导航栏
+
+#  以下为显示标题画布
     FloatLayout:
         canvas:
             Color:
-                rgba: 0.38, 0.49, 0.55, 1 # 前面三个对应红、绿、蓝，最后一个为透明度80%，所有值为0-1之间
-            # 用画矩形函数画一个矩形填补下面圆角边框里面的颜色
+                rgba: 0.08, 0.41, 0.76, 1 # 前面三个对应红、绿、蓝，最后一个为透明度80%，所有值为0-1之间
             Rectangle:
-                pos:7, 193
-                size: 324, 366
-            Line: #  画线函数
+                pos:7, 554
+                size: 324, 21
+            #  用矩形函数画一条线来填补下面画圆角时上面两个角弧度为零时出现的两个缺口
+            Rectangle:
+                pos:2, 534
+                size: 334, 25
+            Line: #  画线函数画一个圆角
                 #  前两位表示矩形的左下角位置，三四位表示宽度和高度，5-8是两个边框之间的像素数（圆角度数），最后一位用于在每个角绘制圆弧的线段数（默认为30），越大越圆润
-                rounded_rectangle:7, 193, 324, 366, 3, 3, 3, 3, 30
+                rounded_rectangle:7, 554, 324, 21, 0, 0, 3, 3, 30
                 #  宽度设置为以宽度为1时为中心，向两边扩展，如果width为50的话，就是向线两边扩展50的宽度
                 width:5
-        Label:
-            text: 'RAN         DOM'
-            font_size:16
-            pos:90, 210
-            size_hint: .19, .055
-            color: 1, 1, 1, 1
 
-#  以下为显示历史数据画布
+#  以下为显示历史数据画布，为了指纹按钮能浮现在历史数据画布上，需要将指纹按钮所在的随机数结果画布拍到后面，拥有更高的显示优先级
     FloatLayout:
         canvas:
             Color:
-                rgba: 0.27, 0.36, 0.4, 1 # 前面三个对应红、绿、蓝，最后一个为透明度80%，所有值为0-1之间
+                rgba: 0.05, 0.29, 0.64, 1 # 前面三个对应红、绿、蓝，最后一个为透明度80%，所有值为0-1之间
             Rectangle:
                 pos:7, 55
                 size: 324, 138
-            #  画一条线来填补下面画圆角时上面两个角弧度为零时出现的两个缺口
-            #Line:
-                points: [431, 8, 278, 8]
-                width:5
-            Line: #  画线函数
+            #  用矩形函数画一条线来填补下面画圆角时上面两个角弧度为零时出现的两个缺口
+            Rectangle:
+                pos:2, 193
+                size: 334, 5
+            Line: #  画线函数画一个圆角
                 #  前两位表示矩形的左下角位置，三四位表示宽度和高度，5-8是两个边框之间的像素数（圆角度数），最后一位用于在每个角绘制圆弧的线段数（默认为30），越大越圆润
                 rounded_rectangle:7, 55, 324, 138, 3, 3, 0, 0, 30
                 #  宽度设置为以宽度为1时为中心，向两边扩展，如果width为50的话，就是向线两边扩展50的宽度
                 width:5
+
+#  以下为显示随机数结果画布
+    FloatLayout:
+        canvas:
+            Color:
+                rgba: 0.13, 0.61, 0.96, 1 # 前面三个对应红、绿、蓝，最后一个为透明度80%，所有值为0-1之间
+            #  用画矩形函数画一个矩形填补下面圆角边框里面的颜色
+            Rectangle:
+                pos:2, 193
+                size: 334, 346
+            #  导入图片
+        Button:
+            background_normal: '2.png'
+            background_down: '2.png'
+            pos:137, 170
+            size_hint:None,None #  要使用确定的像素大小的话就要将大小比例参数关闭
+            size:60, 60
+        Label:
+            text: 'RAN             DOM'
+            font_size:22
+            pos:94, 202
+            #size_hint: .19, .055
+            size_hint:None,None #  要使用确定的像素大小的话就要将大小比例参数关闭
+            size:150, 10
+            color: 1, 1, 1, 1
 
 #  以下为按钮、输入区画布
     FloatLayout:
@@ -107,40 +131,50 @@ Builder.load_string('''
                 size: 338, 50
 
 #  用画布来生成一条线
-        canvas:
+        canvas.after:
             Color:
                 rgba: 0, 0, 0, 1 # 前面三个对应红、绿、蓝，最后一个为透明度100%，所有值为0-1之间
-            Rectangle:
-                pos:2, 46
-                size: 334, 1
+            #  用矩形画一条线
+            #Rectangle:
+            #    pos:2, 46
+            #    size: 334, 1
+            Line:
+                points: [2, 46, 334, 46]
+                width:0.5
 
 #  在输入框下生成一根线提示输入，Line函数必须在canvas画布函数下才行
             Line: #  Min_No
-                points: [126, 8, 172, 8]
+                points: [120, 8, 172, 8]
                 width:1
             Line: #  Max_No
-                points: [232, 8, 278, 8]
+                points: [226, 8, 278, 8]
                 width:1
         Label:
             text: 'Clear'
             font_size:18
-            pos:15, 20
-            size_hint: .06, .01
+            pos:25, 20
+            #size_hint: .06, .01
+            size_hint:None,None #  要使用确定的像素大小的话就要将大小比例参数关闭
+            size:20.28, 0.5
             color: 0, 0, 0, 1
 
         Label:
             text: 'Min:'
             font_size:18
-            pos:81, 20
-            size_hint: .06, .01
+            pos:86, 20
+            #size_hint: .06, .01
+            size_hint:None,None #  要使用确定的像素大小的话就要将大小比例参数关闭
+            size:20.28, 0.5
             color: 0, 0, 0, 1
 
 # 通过设置background_normal和background_active为空将输入框边框取消
         TextInput:
             id: my_textinput1 #  设定id为my_textinput，方便后续调用
-            font_size:16
-            pos:120, 8
-            size_hint: .07, .18
+            font_size:18
+            pos:115, 6
+            #size_hint: .07, .18
+            size_hint:None,None #  要使用确定的像素大小的话就要将大小比例参数关闭
+            size:62, 30
             #  text: 'Min' #  定义默认的文本内容
             multiline: False #  设置输入框为单行，不能换行
             background_normal:'' #  输入框未激活时TextInput的背景图像
@@ -149,15 +183,19 @@ Builder.load_string('''
         Label:
             text: 'Max:'
             font_size:18
-            pos:184, 20
-            size_hint: .06, .01
+            pos:192, 20
+            #size_hint: .06, .01
+            size_hint:None,None #  要使用确定的像素大小的话就要将大小比例参数关闭
+            size:20.28, 0.5
             color: 0, 0, 0, 1
 
         TextInput:
             id: my_textinput2 #  设定id为my_textinput，方便后续调用
-            font_size:16
-            pos:225, 8
-            size_hint: .07, .18
+            font_size:18
+            pos:220, 6
+            #size_hint: .07, .18
+            size_hint:None,None #  要使用确定的像素大小的话就要将大小比例参数关闭
+            size:62, 30
             #  text: 'Max' #  定义默认的文本内容
             multiline: False
             background_normal:'' #  输入框未激活时TextInput的背景图像
@@ -166,8 +204,10 @@ Builder.load_string('''
         Label:
             text: 'Set'
             font_size:18
-            pos:280, 20
-            size_hint: .06, .01
+            pos:300, 20
+            #size_hint: .06, .01
+            size_hint:None,None #  要使用确定的像素大小的话就要将大小比例参数关闭
+            size:20.28, 0.5
             color: 0, 0, 0, 1
 ''')
 
