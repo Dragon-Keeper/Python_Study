bebb326028c503fb06bf4040821acb3255d6ef94
Dragon-Keeper <44353148+Dragon-Keeper@users.noreply.github.com>
2018-11-27 19:09:14 +0800

    del

--- del.py
+++
@@ -1,9 +0,0 @@
-def fahrenheit_converter(C):
-     fahrenheit = C * 9/5 + 32
-     return int(fahrenheit)
-
-b = fahrenheit_converter(20)
-print(fahrenheit_converter(5))
-print(b)
-a = fahrenheit_converter(5)
-print(type(b))
