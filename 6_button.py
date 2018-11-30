d185a061d56fe354ea7000b4e33a7ff8b2b7a20c
Dragon-Keeper <44353148+Dragon-Keeper@users.noreply.github.com>
2018-11-30 12:02:01 +0800

    添加了解读注释

--- 6_button.py
+++ 6_button.py
@@ -5,9 +5,9 @@ from kivy.uix.button import Button
 from kivy.graphics import Color, Ellipse, Line
 
 
-class MyPaintWidget(Widget):
+class MyPaintWidget(Widget):  # 定义一个类
 
-    def on_touch_down(self, touch):
+    def on_touch_down(self, touch):  # 定义一个事件的处理办法
         color = (random(), 1, 1)
         with self.canvas:
             Color(*color, mode='hsv')
@@ -19,20 +19,21 @@ class MyPaintWidget(Widget):
         touch.ud['line'].points += [touch.x, touch.y]
 
 
-class MyPaintApp(App):
+class MyPaintApp(App):  # 定义一个类，继承App类
 
-    def build(self):
-        parent = Widget()
-        self.painter = MyPaintWidget()
-        clearbtn = Button(text='Clear')
+    def build(self):  # 实现它的build()方法，它能返回一个部件的实例（你的部件树的根部件）
+        parent = Widget()  # 定义父空间为Widget()
+        self.painter = MyPaintWidget()  # 定义painter承接MyPaintWidget()这个类的内容
+        clearbtn = Button(text='Clear')  # 定义一个按键，并且上面文字为Clear，将它连接到clearbtn上
+        # 将类clear_canvas(self, obj)被点击（on_release）事件绑定到按键Clear上
         clearbtn.bind(on_release=self.clear_canvas)
-        parent.add_widget(self.painter)
-        parent.add_widget(clearbtn)
-        return parent
+        parent.add_widget(self.painter)  # 在父空间里添加MyPaintWidget()这个类的内容
+        parent.add_widget(clearbtn)  # 在父空间里添加Clear按钮
+        return parent  # 返回父空间内容并显示
 
-    def clear_canvas(self, obj):
-        self.painter.canvas.clear()
+    def clear_canvas(self, obj):  # 定义了一个类用于清空画布
+        self.painter.canvas.clear()  # 每当调用这个函数时，清空画布内容
 
 
 if __name__ == '__main__':
-    MyPaintApp().run()
+    MyPaintApp().run()  # 实例化该类，同时调用它的run()方法
--- on_textedit_event.py
+++ on_textedit_event.py
@@ -7,37 +7,36 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.properties import StringProperty
-from kivy.core.text import LabelBase, DEFAULT_FONT
 from kivy.uix.textinput import TextInput
 from kivy.base import EventLoop
 
 
-class TextInputIME(TextInput):
-    testtext = StringProperty()
+# 为了使用属性，你必须在一个类里面声明它们，注意是直接写在类里面，而不是写在任何的类的方法里，它是一个类属性。每一个属性都默认提供了一个on_<propertyname>事件；当属性值的状态和属性值发生改变时该事件均会被调用。
+class TextInputIME(TextInput):  # 这个类用于接收输入的变化然后赋值
+    testtext = StringProperty()  # 将testtext定义为一个字符属性
 
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs):  # （此句格式固定）要创建一个自定义事件，需要在一个类中注册事件名，并创建一个同名的方法
+        # （此句格式固定）只需这里的TextInputIME与上面类名TextInputIME同名即可
         super(TextInputIME, self).__init__(**kwargs)
-        EventLoop.window.bind(on_textedit=self._on_textedit)
+        EventLoop.window.bind(on_textedit=self._on_textedit)  # 添加下面_on_textedit事件的处理方法：on_textedit
+        # 触屏消息的处理是在函数开始的 EventLoop.idle() 里进行的
+        # bind:将一个方法绑定到一个消息类型,用于保存方法的是一个list类型
 
-    def _on_textedit(self, window, text):
-        self.testtext = text
+    def _on_textedit(self, window, text):  # 这个是这个属性里的textedit（文件编辑）事件
+        self.testtext = text  # 每次发生文件编辑事件时，将text的值赋值给testtext
+
+    # 好，上面这个类注册了消息类型和消息的处理过程，按下来就要等消息上门了。
 
 
-class MainWidget(Widget):
-    text = StringProperty()
+class MainWidget(Widget):  # 这个类用于接收输入的赋值然后显示出来
+    text = StringProperty()  # 将text定义为一个字符属性
 
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
-        self.text = ''
+        self.text = ''  # 创建一个自定义事件,设定text的值为空，等待赋值
 
-    def confim(self):
-        self.text = self.ids["text_box"].text
-
-    def changeFont(self):
-        try:
-            LabelBase.register(DEFAULT_FONT, self.ids["text_font"].text)
-        except Exception:
-            self.ids["text_font"].text = "can't load font."
+    def confim(self):  # 用来将输入的内容显示在label上，关联button的on_press
+        self.text = self.ids["text_box"].text  # 使用ids来访问带id标识的对象，将text_box赋值给text，而text_box取值自输入栏
 
 
 class TextEditTestApp(App):
@@ -54,46 +53,23 @@ if __name__ == '__main__':
     BoxLayout:
         orientation: 'vertical'
         size: root.size
-        BoxLayout:
-            Label:
-                size_hint_x: 3
-                text: "Multi language font file path"
-            TextInput:
-                id: text_font
-                size_hint_x: 5
-            Button:
-                size_hint_x: 2
-                text: "Change Font"
-                on_press: root.changeFont()
-        BoxLayout:
-            Label:
-                size_hint_x: 3
-                text: "Text editing by IME"
-            Label:
-                size_hint_x: 7
-                text:text_box.testtext
-                canvas.before:
-                    Color:
-                        rgb: 0.5765 ,0.5765 ,0.5843
-                    Rectangle:
-                        pos: self.pos
-                        size: self.size
+
         BoxLayout:
             Label:
                 size_hint_x: 3
                 text: "Enter text ->"
             TextInputIME:
-                id: text_box
+                id: text_box  # 设定id，用来给confim函数取值
                 size_hint_x: 7
                 focus: True
         BoxLayout:
             Button:
                 size_hint_x: 3
                 text: "Confirm text property"
-                on_press: root.confim()
+                on_press: root.confim()  # 如果点击，则调用confim函数
             Label:
                 size_hint_x: 7
-                text: root.text
+                text: root.text  # 从根层次取值text，就是上面confim函数的作用
                 canvas.before:
                     Color:
                         rgb: 0.5765 ,0.5765 ,0.5843
