053ee6b29c89f54518d02169808c2a0cd5f6ec3f
Dragon-Keeper <44353148+Dragon-Keeper@users.noreply.github.com>
2018-12-04 20:37:18 +0800

    完成，准备添加点击clear是弹出确认按钮；读写数据文件

--- main.py
+++ main.py
@@ -16,32 +16,6 @@ kivy.require('1.10.1')
 #  from kivy.graphics import Color, Rectangle  # 为背景添加颜色，画矩形
 #  from kivy.config import Config
 
-# --------------------------------以下为随机函数--------------------------------
-
-
-set_no = set()
-
-
-def ran_no(min_no_input, max_no_input):
-    # 由于input函数输入的是str字符串型，必须转换为整数型才能进行随机运算，直接用int转换的话提示错误，所以先用float转换为浮点型，然后再用round取整去小数点
-    min_no = round(float(min_no_input))
-    max_no = round(float(max_no_input))
-    random_no = random.randint(min_no, max_no)  # 产生范围内的随机数
-    if random_no not in set_no:  # 判断随机数是否已存在，如果不存在则写入集合
-        print('新的随机数生成：' + str(random_no))  # 调试用的输出
-    else:
-        Random_Times = 1  # 定义函数用来计算随机次数
-        while random_no in set_no:  # 判断随机数是否已存在，如果存在则重新产生随机数直到不存在
-            print(str(random_no) + '：已经存在')  # 调试用的输出
-            random_no = random.randint(min_no, max_no)  # 产生范围内的随机数
-            Random_Times = Random_Times + 1  # 每次循环次数加一
-            Max_Random_Times = max_no**2  # 循环输入数值最大数的平方次数
-            if Random_Times > Max_Random_Times:  # 当循环次数大于输入数值最大数的平方次数时
-                break  # 退出循环，要求从新输入范围
-        else:  # 经过循环重新产生不存在的随机数，然后写入集合
-            print('新的随机数生成：' + str(random_no))  # 调试用的输出
-    return random_no
-# --------------------------------以上为随机函数--------------------------------
 # -----------------------------以下为监测输入框内容等待赋值-----------------------
 # 为了使用属性，你必须在一个类里面声明它们，注意是直接写在类里面，而不是写在任何的类的方法里，它是一个类属性。每一个属性都默认提供了一个on_<propertyname>事件；当属性值的状态和属性值发生改变时该事件均会被调用。
 
@@ -76,6 +50,7 @@ class BackGround(FloatLayout):  # 这个类用于接收输入的赋值然后显�
         self.ran_no = ''
         self.his_no_sort = ''
         self.his_no = ''
+        self.set_no = set()
 
     def confim(self):  # 用来将输入的内容显示在label上，关联button的on_press
         # 使用ids来访问带id标识的对象，将text_box赋值给text，而text_box取值自输入栏
@@ -88,20 +63,44 @@ class BackGround(FloatLayout):  # 这个类用于接收输入的赋值然后显�
         else:
             max = self.min_no_input
             min = self.max_no_input
-        r_n = ran_no(min, max)
-        set_no.add(r_n)  # 随机数写入集合
-        list_set = list(set_no)  # 将集合转换成列表
+# ------------------------------以下为随机数生成过程------------------------------
+        # 由于input函数输入的是str字符串型，必须转换为整数型才能进行随机运算，直接用int转换的话提示错误，所以先用float转换为浮点型，然后再用round取整去小数点
+        min_no = round(float(min))
+        max_no = round(float(max))
+        random_no = random.randint(min_no, max_no)  # 产生范围内的随机数
+        if random_no not in self.set_no:  # 判断随机数是否已存在，如果不存在则写入集合
+            print('新的随机数生成：' + str(random_no))  # 调试用的输出
+        else:
+            Random_Times = 1  # 定义函数用来计算随机次数
+            while random_no in self.set_no:  # 判断随机数是否已存在，如果存在则重新产生随机数直到不存在
+                print(str(random_no) + '：已经存在')  # 调试用的输出
+                random_no = random.randint(min_no, max_no)  # 产生范围内的随机数
+                Random_Times = Random_Times + 1  # 每次循环次数加一
+                Max_Random_Times = max_no**2  # 循环输入数值最大数的平方次数
+                if Random_Times > Max_Random_Times:  # 当循环次数大于输入数值最大数的平方次数时
+                    break  # 退出循环，要求从新输入范围
+            else:  # 经过循环重新产生不存在的随机数，然后写入集合
+                print('新的随机数经过循环生成：' + str(random_no))  # 调试用的输出
+# ------------------------------以上为随机数生成过程------------------------------
+        r_n = random_no
+        self.set_no.add(r_n)  # 随机数写入集合
+        list_set = list(self.set_no)  # 将集合转换成列表
         self.ran_no = str(r_n)  # 显示随机数
         self.his_no = self.his_no + str(r_n) + '->'  # 按随机数产生顺序显示随机结果
         list_set.sort()  # 列表按小到大排序，这样输出到数据文件就是顺序的了
         self.his_no_sort = str(list_set).replace('[', '').replace(
             ']', '').replace(',', ' ').replace('\'', '')
-        list_set.clear()
+        # 通过计算列表内容的数量和"max"对比，相等时在非排序历史数据栏提示随机完毕
+        if len(list_set) == max_no:
+            self.his_no = self.his_no + 'My Lord! Random Finish ^_^'
+        else:
+            pass
 
     def confim2(self):  # 用来将输入的内容显示在label上，关联button的on_press
         self.ran_no = ' '
         self.his_no_sort = ' '
         self.his_no = ' '
+        self.set_no.clear()  # 通过清除集合内容来达到清除历史数据排序显示
 
 # -----------------------------以上为将输入框内容赋值给具体函数--------------------
 
