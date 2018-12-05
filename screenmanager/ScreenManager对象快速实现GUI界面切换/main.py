d9add032f17e583e9cee44d4de5fdf9c237c1548
Dragon-Keeper <44353148+Dragon-Keeper@users.noreply.github.com>
2018-12-05 13:02:28 +0800

    窗口切换素材

---
+++ screenmanager/ScreenManager对象快速实现GUI界面切换/main.py
@@ -0,0 +1,28 @@
+from kivy.app import App
+# from kivy.uix.boxlayout import BoxLayout
+# from kivy.properties import ObjectProperty
+# from kivy.uix.label import Label
+# from kivy.factory import Factory
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class MainScreen(Screen):
+    pass
+
+
+class SubScreen(Screen):
+    pass
+
+
+class ScreenApp(App):
+
+    def build(self):
+        sm = ScreenManager()
+        scm = MainScreen(name="main")
+        scs = SubScreen(name="sub")
+        sm.add_widget(scm)
+        sm.add_widget(scs)
+        return sm
+
+
+ScreenApp().run()
